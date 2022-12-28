75de1866a3bdd385729717c0a514d594be562e26
khituras <chew@gmx.net>
2022-12-28 16:45:20 +0100

    Resolves #5.

---
+++ es-docker/README.txt
@@ -0,0 +1,10 @@
+This creates a docker image for a development ElasticSearch
+instance that has the preanalyzed plugin installed:
+docker build . -t elasticsearch-preanalyzed-smith:<tag>
+
+Create and run a container based on the image:
+docker run -d --name elasticsearch-preanalyzed-smith -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch-preanalyzed-smith:<tag>
+
+And, finally, create an index:
+curl -XPUT http://localhost:9200/smith -H 'Content-Type: application/json' -d @../engine/smithsearch/src/main/resources/esMappingFile.json 
+
---
+++ indexing-pipeline/src/main/java/de/julielab/smithsearch/index/SmithSearchFieldGenerator.java
@@ -0,0 +1,38 @@
+package de.julielab.smithsearch.index;
+
+import de.julielab.jcore.consumer.es.*;
+import de.julielab.jcore.consumer.es.preanalyzed.Document;
+import de.julielab.jcore.consumer.es.preanalyzed.PreanalyzedFieldValue;
+import de.julielab.jcore.consumer.es.preanalyzed.PreanalyzedToken;
+import de.julielab.jcore.types.EntityMention;
+import de.julielab.jcore.utility.JCoReTools;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.jcas.JCas;
+
+import java.util.List;
+
+public class SmithSearchFieldGenerator extends FieldGenerator {
+    public SmithSearchFieldGenerator(FilterRegistry filterRegistry) {
+        super(filterRegistry);
+    }
+
+    @Override
+    public Document addFields(JCas aJCas, Document doc) throws FieldGenerationException {
+        final String docId = JCoReTools.getDocId(aJCas);
+        if (docId == null)
+            throw new FieldGenerationException(new IllegalArgumentException("CAS does not have a docId."));
+
+        final FeaturePathSets entityIdFeatureSet = new FeaturePathSets(new FeaturePathSet(EntityMention.type, List.of("/specificType")));
+        final List<PreanalyzedToken> preanalyzedTokens;
+        try {
+            preanalyzedTokens = getTokensForAnnotationIndexes(entityIdFeatureSet, null, true, PreanalyzedToken.class, null, null, aJCas);
+        } catch (CASException e) {
+            log.error("Creation of preanalyzed tokens failed.", e);
+            throw new FieldGenerationException(e);
+        }
+        final PreanalyzedFieldValue fieldValue = new PreanalyzedFieldValue(aJCas.getDocumentText(), preanalyzedTokens);
+        doc.addField("doc_id", docId);
+        doc.addField("text", fieldValue);
+        return doc;
+    }
+}
